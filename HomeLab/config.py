c561327b9864a31fd918c7f5811d4e106eae0cc6
leomath42 <leonardomatheus042@gmail.com>
2020-04-22 09:15:49 -0300

    Arquivo de configurações temporário.

---
+++ HomeLab/config.py
@@ -0,0 +1,10 @@
+'''
+    Arquivo de configurações v 0.1 via dicionário Python
+    Arquivo de configurações v 0.2 será via xml
+'''
+
+config = \
+{
+    'engine_name' : "sqlite:////home/mohelot/projetos/home_lab/bancos/homelab.db",
+    'engine_echo' : True
+}
